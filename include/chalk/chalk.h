08fe1e0c56baf3db6276d6a752c3c13421aa3c17
ebenali@tradeterminal.io <ebenali@tradeterminal.io>
2019-11-23 21:53:49 -0500

    make ostlyer decl static

--- include/chalk/chalk.h
+++ include/chalk/chalk.h
@@ -203,7 +203,7 @@ constexpr auto Conceal = Format{"\033[8m", "\033[28m"};
 constexpr auto CrossedOut = Format{"\033[9m", "\033[29m"};
 };  // namespace fmt
 
-OStreamStyler ostyler{&std::cout};
+static OStreamStyler ostyler{&std::cout};
 
 };  // namespace chalk
 
